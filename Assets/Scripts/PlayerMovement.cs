2e739e969f231ef6a1063d81957a780f3fc14bad
ajjepson <156846692+ajjepson@users.noreply.github.com>
2024-05-07 11:56:55 -0400

    Bandage Fixing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -121,7 +121,7 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.5f, whatIsGround);
 
         MyInput();
         SpeedControl();
